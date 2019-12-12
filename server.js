42862ee10408aeca706b65572e78cdfe1b660b80
negupta0 <58423841+negupta0@users.noreply.github.com>
2019-12-12 13:41:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 	
+// default TRIAL route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello TIFFANY' })
+});
 
 // default route
 app.get('/', function (req, res) {
     return res.send({ error: true, message: 'hello' })
 });
 
-// default TRIAL route
-app.get('/', function (req, res) {
-    return res.send({ error: true, message: 'hello TIFFANY' })
-});
 
 //mysql configuration
 var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'mysql.database-check.svc.cluster.local';
